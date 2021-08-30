fe9afe6280fe6e9e76b2b011baeda78c615c5c5a
gmukul01 <mukulbansal01@gmail.com>
2021-08-30 13:12:42 +0530

    fix: regenerator error in medly app
    affects: @medly/create-app

--- packages/app/template/common/src/index.tsx
+++ packages/app/template/common/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
+import 'regenerator-runtime/runtime';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));

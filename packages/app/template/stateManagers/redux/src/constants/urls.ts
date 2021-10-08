23c9b972a7b5afd91692b260b0be3da690e88348
Manish Ranjan <manish@Manishs-MacBook-Pro.local>
2021-10-08 13:19:36 +0530

    fix: fix null type in redux starter template
    affects: @medly/create-app

--- packages/app/template/stateManagers/redux/src/constants/urls.ts
+++ packages/app/template/stateManagers/redux/src/constants/urls.ts
@@ -1,6 +1,6 @@
 export const API_URLS = (key: string, params?: Record<string, unknown>): string => {
     const urls: Record<string, string> = {
-        USER: `https://run.mocky.io/v3/${params.id}`
+        USER: `https://run.mocky.io/v3/${params?.id}`
     };
     return urls[key];
 };
--- packages/app/template/stateManagers/redux/src/store/user/reducer.ts
+++ packages/app/template/stateManagers/redux/src/store/user/reducer.ts
@@ -1,7 +1,12 @@
 import { Reducer } from 'redux';
 import { User, UserActions, UserActionTypes } from './types';
 
-export const initialState: User = null;
+export const initialState: User = {
+    firstName:'',
+    lastName: '',
+    email: '',
+    phone: '',
+};
 
 export const user: Reducer<User, UserActions> = (state = initialState, action) => {
     switch (action.type) {
